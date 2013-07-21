c2f284b259b4543d9e6b33f58364d68797d71241
Zena B <zena667@gmail.com>
2013-07-21 14:57:35 -0400

    Making tests for pagination pas

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,9 @@
 class ProjectsController < ApplicationController
-
+  #for pagination, the page parameter is passed in by the user to view
+  #that particular page number
   def index
-    @projects = Project.all
+    @projects = Project.order("created_at DESC").page(params[:page]).per(8)
+
   end
 
   def home
@@ -12,3 +14,4 @@ class ProjectsController < ApplicationController
   end
 
 end
+
